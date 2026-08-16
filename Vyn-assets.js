53e5ae2688099b7789b52e7b838bd64c8e731a9b
Vyn_O.S <normal8607@gmail.com>
2026-08-16 10:22:40 -0500

    Update assets from admin panel

--- Vyn-assets.js
+++ Vyn-assets.js
@@ -84,7 +84,7 @@ var assetsCreados = [
         "descricao": "A gui resource package",
         "descricaoLonga": "A graphical user interface resource pack that you can use to create your game\n",
         "imagens": [
-            "https://media.discordapp.net/attachments/1522575046768328724/1523152848240509058/image.png?ex=6a8319e1&is=6a81c861&hm=61d4297adf4aed67d16f86591bca5ee672408ed23c42c6799c3db7f1212fddf6&=&format=webp&quality=lossless"
+            "https://i.postimg.cc/6QGdGfg9/imagen.png"
         ],
         "linkDownload": "https://cdn.discordapp.com/attachments/1522575046768328724/1523152956663267399/GUI_PACK.rbxm?ex=6a8319fb&is=6a81c87b&hm=76bae27b6d2885a07faa8c89420eae6b26dfe80ecf0c077a1941f0772a1b39ad&",
         "formato": ".rbxm",
