916982709580c3c8762ea87251c95e2866301341
Vyn_O.S <normal8607@gmail.com>
2026-08-16 00:04:44 -0500

    Update assets from admin panel

---
+++ Vyn-assets.js
@@ -0,0 +1,23 @@
+var assetsCreados = [
+    {
+        "id": 1786856685156,
+        "nome": "Female Body R6 - Female 1",
+        "categoria": [
+            "BODY"
+        ],
+        "descricao": "Female 1 - R6",
+        "descricaoLonga": "FEMALE - Includes: Torso, Legs, Arms and Torso",
+        "imagens": [
+            "https://i.postimg.cc/sfJL7FFH/Female-Body-R6.png",
+            "https://i.postimg.cc/MZYLVkkP/Front.png",
+            "https://i.postimg.cc/nV1WB88P/Back.png",
+            "https://i.postimg.cc/66hPnss1/Left.png",
+            "https://i.postimg.cc/DfcNLVk2/Right.png"
+        ],
+        "linkDownload": "https://www.mediafire.com/file/u01c7z6uahc3isf/FemaleBodyV1.zip/file",
+        "formato": ".ZIP/.mesh",
+        "tamanho": "1.58 MB",
+        "status": "nenhum",
+        "fail": "none"
+    }
+];
