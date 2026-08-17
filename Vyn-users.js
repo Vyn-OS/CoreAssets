371e99f7fafc2f82d2d7ded8ee77ed09fb614a7d
Vyn_O.S <normal8607@gmail.com>
2026-08-16 22:57:17 -0500

    Update Vyn-users.js

--- Vyn-users.js
+++ Vyn-users.js
@@ -1,7 +1,7 @@
 var adminUsers = [
     {
         "id": 1786938242668,
-        "usuario": "vyn",
+        "usuario": "Vyn",
         "deviceId": "dev-mswou1jg-fp3xtfcit8p2pk2h6ewwo1",
         "banned": false,
         "creado": "2026-08-17T03:44:02.668Z"
