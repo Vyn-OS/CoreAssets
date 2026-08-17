0e3669be3b10f0ac49907cc67ec29ee7b9da95c9
Vyn_O.S <normal8607@gmail.com>
2026-08-16 22:51:33 -0500

    Update admin users

--- Vyn-users.js
+++ Vyn-users.js
@@ -5,5 +5,12 @@ var adminUsers = [
         "deviceId": "dev-mswou1jg-fp3xtfcit8p2pk2h6ewwo1",
         "banned": false,
         "creado": "2026-08-17T03:44:02.668Z"
+    },
+    {
+        "id": 1786938694387,
+        "usuario": "kira",
+        "deviceId": "dev-mswotdn3-alhqyw0czzln06acqx50sb",
+        "banned": false,
+        "creado": "2026-08-17T03:51:34.387Z"
     }
 ];
