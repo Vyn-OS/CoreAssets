a0c7e484cac1119c70cf7b17db406e9a2f4e583d
Vyn_O.S <normal8607@gmail.com>
2026-08-21 23:20:39 -0500

    Update assets from admin panel

--- Vyn-assets.js
+++ Vyn-assets.js
@@ -114,23 +114,5 @@ var assetsCreados = [
         "status": "nenhum",
         "fail": "none",
         "autor": "Vyn"
-    },
-    {
-        "id": 1787372338206,
-        "nome": "Test",
-        "categoria": [
-            "Test"
-        ],
-        "descricao": "Test",
-        "descricaoLonga": "Test",
-        "imagens": [
-            "Test"
-        ],
-        "linkDownload": "Test",
-        "formato": "Test",
-        "tamanho": "Test",
-        "status": "nenhum",
-        "fail": "none",
-        "autor": "Mod"
     }
 ];
