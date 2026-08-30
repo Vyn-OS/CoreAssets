8e5e135c55212e26305634806e2bc0727a64af4b
Vyn_O.S <normal8607@gmail.com>
2026-08-30 00:21:04 -0500

    Approve pending asset: Test

--- Vyn-assets.js
+++ Vyn-assets.js
@@ -114,5 +114,23 @@ var assetsCreados = [
         "status": "nenhum",
         "fail": "none",
         "autor": "Vyn"
+    },
+    {
+        "id": 1787373736486,
+        "nome": "Test",
+        "categoria": [
+            "Test"
+        ],
+        "descricao": "Test",
+        "descricaoLonga": "Test",
+        "imagens": [
+            "Test"
+        ],
+        "linkDownload": "Test",
+        "formato": "Test",
+        "tamanho": "Test",
+        "status": "nenhum",
+        "fail": "none",
+        "autor": "Mod"
     }
 ];
